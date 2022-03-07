a9301e3f969b893325c6e0b25d87db00fe0733ea
B1900812 <99552857+B1900812@users.noreply.github.com>
2022-03-07 10:29:04 +0800

    Add files via upload

---
+++ BIT216A2/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="app-container">
+    <router-view />
+  </div>
+</template>
+
+<script>
+import { defineComponent, onMounted, reactive, toRefs } from 'vue'
+import { localSet } from './utils'
+
+export default defineComponent({
+  setup() {
+    const state=reactive({
+
+    })
+    onMounted(()=>{
+      localSet("isLogin",false)
+      localSet('userList',[{username: 'admin',password:'123456'}])
+    })
+    return {
+      ...toRefs(state)
+    }
+  },
+})
+</script>
+<style scoped>
+</style>
+
